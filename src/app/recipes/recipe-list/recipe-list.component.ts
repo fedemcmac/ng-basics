33a57466c53f38acbeb921fd7cfef0a6fe59388b
Federica Macchi <federica.macchi@greenflag.com>
2020-09-07 17:27:49 +0100

    Added Ingredient class

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: [];
-  
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMYPNgIJspfvZ-GYCqDjmxBDUpX1CP9J2S-g&usqp=CAU'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMYPNgIJspfvZ-GYCqDjmxBDUpX1CP9J2S-g&usqp=CAU')
+  ];
+
   constructor() { }
 
   ngOnInit() {
